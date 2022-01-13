98a17d2de5483a7369584bf6fe9a7f99ff20263a
kory-job <catcher.intherye.rk@gmail.com>
2022-01-13 12:57:19 +0900

    api/edit_dev_environment

---
+++ api/config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	"log"
+
+	"github.com/kory-jp/react_golang_mux/api/utils"
+	"gopkg.in/ini.v1"
+)
+
+type ConfigList struct {
+	Port      string
+	LogFile   string
+	SQLDriver string
+	UserName  string
+	Password  string
+	DBPort    string
+	DBname    string
+	Static    string
+}
+
+var Config ConfigList
+
+func init() {
+	LoadConfig()
+	utils.LoggingSettings(Config.LogFile)
+}
+
+func LoadConfig() {
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	Config = ConfigList{
+		Port:      cfg.Section("api").Key("port").MustString("8000"),
+		LogFile:   cfg.Section("api").Key("logfile").String(),
+		SQLDriver: cfg.Section("db").Key("driver").String(),
+		UserName:  cfg.Section("db").Key("user_name").String(),
+		Password:  cfg.Section("db").Key("password").String(),
+		DBPort:    cfg.Section("db").Key("port").String(),
+		DBname:    cfg.Section("db").Key("db_name").String(),
+		Static:    cfg.Section("api").Key("static").String(),
+	}
+}
