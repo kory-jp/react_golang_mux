221bee93767720c3adf6fbe82b78c55db99a5d7e
kory-job <catcher.intherye.rk@gmail.com>
2022-04-01 17:44:24 +0900

    testUpdate

--- api/interfaces/controllers/todo_controller_test.go
+++ api/interfaces/controllers/todo_controller_test.go
@@ -497,3 +497,225 @@ func TestShow(t *testing.T) {
 		})
 	}
 }
+
+func TestUpdate(t *testing.T) {
+	c := gomock.NewController(t)
+	defer c.Finish()
+	sqlhandler := mock_database.NewMockSqlHandler(c)
+	ctrl := controllers.NewTodoController(sqlhandler)
+	result := mock_database.NewMockResult(c)
+	statement := `
+		update
+			todos
+		set
+			title = ?,
+			content = ?,
+			image_path = ?
+		where
+			id = ?
+		and
+			user_id = ?
+	`
+
+	cases := []struct {
+		name            string
+		args            domain.Todo
+		loginUserId     int
+		isImage         bool
+		prepareMockFn   func(m *mock_database.MockSqlHandler, r *mock_database.MockResult, statement string, todo domain.Todo)
+		responseMessage string
+	}{
+		{
+			name: "必須項目が入力された場合(画像有り)、データ保存成功",
+			args: domain.Todo{
+				ID:        1,
+				UserID:    1,
+				Title:     "test title",
+				Content:   "test content",
+				ImagePath: "test.png",
+			},
+			loginUserId: 1,
+			isImage:     true,
+			prepareMockFn: func(m *mock_database.MockSqlHandler, r *mock_database.MockResult, statement string, args domain.Todo) {
+				r.EXPECT().RowsAffected().Return(int64(0), nil).AnyTimes()
+				m.EXPECT().
+					Execute(statement, args.Title, args.Content, gomock.Any(), args.ID, args.UserID).
+					Return(r, nil)
+			},
+			responseMessage: "更新しました",
+		},
+		{
+			name: "必須項目が入力された場合(画像無し),データ保存成功",
+			args: domain.Todo{
+				ID:      1,
+				UserID:  1,
+				Title:   "test title",
+				Content: "test content",
+			},
+			loginUserId: 1,
+			isImage:     false,
+			prepareMockFn: func(m *mock_database.MockSqlHandler, r *mock_database.MockResult, statement string, args domain.Todo) {
+				r.EXPECT().RowsAffected().Return(int64(0), nil).AnyTimes()
+				m.EXPECT().
+					Execute(statement, args.Title, args.Content, gomock.Any(), args.ID, args.UserID).
+					Return(r, nil)
+			},
+			responseMessage: "更新しました",
+		},
+		{
+			name: "ログイン状態が確認できない場合,データ保存失敗",
+			args: domain.Todo{
+				ID:      1,
+				UserID:  1,
+				Title:   "test title",
+				Content: "test content",
+			},
+			loginUserId: 0,
+			isImage:     false,
+			prepareMockFn: func(m *mock_database.MockSqlHandler, r *mock_database.MockResult, statement string, args domain.Todo) {
+				r.EXPECT().RowsAffected().Return(int64(0), nil).AnyTimes()
+				m.EXPECT().
+					Execute(statement, args.Title, args.Content, gomock.Any(), args.ID, args.UserID).
+					Return(r, nil).
+					AnyTimes()
+			},
+			responseMessage: "ログインしてください",
+		},
+		{
+			name: "タイトルが0文字の場合,データ保存失敗",
+			args: domain.Todo{
+				ID:      1,
+				UserID:  1,
+				Title:   "",
+				Content: "test content",
+			},
+			loginUserId: 1,
+			isImage:     false,
+			prepareMockFn: func(m *mock_database.MockSqlHandler, r *mock_database.MockResult, statement string, args domain.Todo) {
+				r.EXPECT().RowsAffected().Return(int64(0), nil).AnyTimes()
+				m.EXPECT().
+					Execute(statement, args.Title, args.Content, gomock.Any(), args.ID, args.UserID).
+					Return(r, nil).
+					AnyTimes()
+			},
+			responseMessage: "タイトルは必須です。",
+		},
+		{
+			name: "タイトルが50文字以上の場合,データ保存失敗",
+			args: domain.Todo{
+				ID:      1,
+				UserID:  1,
+				Title:   strings.Repeat("t", 51),
+				Content: "test content",
+			},
+			loginUserId: 1,
+			isImage:     false,
+			prepareMockFn: func(m *mock_database.MockSqlHandler, r *mock_database.MockResult, statement string, args domain.Todo) {
+				r.EXPECT().RowsAffected().Return(int64(0), nil).AnyTimes()
+				m.EXPECT().
+					Execute(statement, args.Title, args.Content, gomock.Any(), args.ID, args.UserID).
+					Return(r, nil).
+					AnyTimes()
+			},
+			responseMessage: "タイトルは50文字未満の入力になります。",
+		},
+		{
+			name: "メモが2001文字以上の場合,データ保存失敗",
+			args: domain.Todo{
+				ID:      1,
+				UserID:  1,
+				Title:   "test title",
+				Content: strings.Repeat("t", 2001),
+			},
+			loginUserId: 1,
+			isImage:     false,
+			prepareMockFn: func(m *mock_database.MockSqlHandler, r *mock_database.MockResult, statement string, args domain.Todo) {
+				r.EXPECT().RowsAffected().Return(int64(0), nil).AnyTimes()
+				m.EXPECT().
+					Execute(statement, args.Title, args.Content, gomock.Any(), args.ID, args.UserID).
+					Return(r, nil).
+					AnyTimes()
+			},
+			responseMessage: "メモは2000文字を超えて入力はできません。",
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			var buffer bytes.Buffer
+			writer := multipart.NewWriter(&buffer)
+
+			// --- 画像データ ---
+			if tt.isImage {
+				fileWriter, err := writer.CreateFormFile("image", tt.args.ImagePath)
+				if err != nil {
+					t.Fatalf("Failed to create file writer. %s", err)
+				}
+
+				imgPath := "../../assets/test/img/" + tt.args.ImagePath
+				readFile, err := os.Open(imgPath)
+				if err != nil {
+					t.Fatalf("Failde to create file writer. %s", err)
+				}
+				defer readFile.Close()
+				io.Copy(fileWriter, readFile)
+			}
+
+			// --- タイトルデータ ---
+			titleWriter, err := writer.CreateFormField("title")
+			if err != nil {
+				t.Fatalf("Failed to create file writer. %s", err)
+			}
+			titleWriter.Write([]byte(tt.args.Title))
+
+			// --- 本文データ ---
+			contentWriter, err := writer.CreateFormField("content")
+			if err != nil {
+				t.Fatalf("Failed to create file writer. %s", err)
+			}
+			contentWriter.Write([]byte(tt.args.Content))
+
+			// --- フィールドの書き込みが終了後にClose ---
+			writer.Close()
+			// ---
+			apiURL := "/api/todos/update/" + strconv.Itoa(tt.args.ID)
+			req := httptest.NewRequest("POST", apiURL, &buffer)
+			req.Header.Add("Content-Type", writer.FormDataContentType())
+			w := httptest.NewRecorder()
+
+			// --- sessionにユーザーIDを保存処理 ---
+			session, err := store.Get(req, "session")
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			session.Values["userId"] = tt.loginUserId
+			err = session.Save(req, w)
+			if err != nil {
+				t.Error(err)
+			}
+
+			// --- mock ---
+			tt.prepareMockFn(sqlhandler, result, statement, tt.args)
+
+			// --- テスト実行 ---
+			ctrl.Update(w, req)
+			var tm TodoMessage
+			buf, _ := ioutil.ReadAll(w.Body)
+			if err = json.Unmarshal(buf, &tm); err != nil {
+				t.Error(err)
+			}
+
+			if w.Code != 200 {
+				t.Error(w.Code)
+			}
+
+			if tm.Message != "" {
+				assert.Equal(t, tm.Message, tt.responseMessage)
+			}
+			if tm.Error != "" {
+				assert.Equal(t, tm.Error, tt.responseMessage)
+			}
+		})
+	}
+}
