571834d8a0fdd3bec6cca3c995c51a053d0eeab6
kory-job <catcher.intherye.rk@gmail.com>
2022-04-27 09:49:12 +0900

    taskCardIndex

--- client/client-app/src/reducks/taskCards/operations.ts
+++ client/client-app/src/reducks/taskCards/operations.ts
@@ -1,7 +1,9 @@
 import { TodayOutlined } from "@mui/icons-material";
 import axios from "axios";
 import { Dispatch } from "react";
+import { nowLoadingState } from "../loading/actions";
 import { pushToast } from "../toasts/actions";
+import { indexTaskCardsAction } from "./actions";
 import { TaskCard, TaskCards } from "./types";
 
 type Response = {
@@ -42,5 +44,38 @@ export const createTaskCard = (todoId: number, title: string, purpose: string, c
         console.log(error)
         dispatch(pushToast({title: '処理に失敗しました', severity: "error"}))
       })
+  }
+}
+
+export const indexTaskCards = (todoId: number, setSumPage: React.Dispatch<React.SetStateAction<number>>, queryPage: number) => {
+  return async(dispatch: Dispatch<{}>) => {
+    dispatch(nowLoadingState(true))
+    const apiURL = process.env.REACT_APP_API_URL + `todo/${todoId}/taskcard?page=${queryPage}`
+    axios
+      .get(apiURL,
+      {
+        withCredentials: true,
+        headers:{
+          'Accept': 'application/json',  
+          'Content-Type': 'multipart/form-data'
+        }
+      }
+      ).then((response) => {
+        const resp: Response = response.data
+        if (resp.status == 200) {
+          dispatch(indexTaskCardsAction(resp.taskCards))
+          setSumPage(Number(resp.sumPage))
+        } else {
+          dispatch(pushToast({title: resp.message, severity: "error"}))
+        }
+      })
+      .catch((error) => {
+        dispatch(pushToast({title: 'データ取得に失敗しました', severity: "error"}))
+      })
+      .finally(() => {
+        setTimeout(() => {
+          dispatch(nowLoadingState(false));
+        }, 800);
+      });
   }
 }
