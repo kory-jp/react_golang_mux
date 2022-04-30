ecaa3a1b87cb6972c873e766acdd23493a7ff00c
kory-job <catcher.intherye.rk@gmail.com>
2022-04-30 10:59:48 +0900

    loginPage

---
+++ client/client-app/src/components_ver2/atoms/images/TopImage.tsx
@@ -0,0 +1,56 @@
+import { CardMedia } from "@mui/material";
+import { Box } from "@mui/system";
+import { FC } from "react";
+import Idea from "../../../assets/images/Idea.jpg"
+import OnlyLogo from "../../../assets/images/OnlyLogo.svg"
+
+export const TopImage: FC = () => {
+  return(
+    <>
+       <Box
+        position='relative'
+        component='header'
+       >
+        <CardMedia 
+          image={Idea}
+          sx={{
+            height: '480px',
+          }}
+        />
+        <Box
+          color="#DADD56"
+          position='absolute'
+          sx={{
+            fontSize: {
+              xs: '40px',
+            },
+            top: '80px',
+            left: {
+              xs: '10%',
+              sm: '30%',
+              md: '40%',
+            }
+          }}
+        >
+          President Todo
+        </Box>
+        <CardMedia 
+          image={OnlyLogo}
+          sx={{
+            height: '80px',
+            width: '56px',
+            position: 'absolute',
+            top: '160px',
+            left: {
+              xs: '40%',
+              sm: '45%',
+              md: '47%'
+            }
+          }}
+        />
+       </Box>
+    </>
+  )
+}
+
+export default TopImage;
--- client/client-app/src/components_ver2/template/users/DefaultTemplate.tsx
+++ client/client-app/src/components_ver2/template/users/DefaultTemplate.tsx
@@ -1,5 +1,9 @@
+import { CardMedia, Grid, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import { ReactNode, VFC } from "react";
+import TopImage from "../../atoms/images/TopImage";
 import Footer from "../../organisms/layout/Footer";
+import AppLogo from "../../../assets/images/AppLogo.svg"
 
 type Props = {
   children: ReactNode
@@ -9,8 +13,115 @@ export const DefaultTemplate: VFC<Props> = (props) => {
   const {children} = props
   return(
     <>
-      {children}
-      <Footer />
+      <Box
+        sx={{
+          marginBottom: {
+            xs: '40px',
+          }
+        }}
+      >
+        <TopImage />
+      </Box>
+      <Box
+        component='main'
+        id='main'
+        display='flex'
+        justifyContent='center'
+        // sx={{
+        //   width: {
+        //     xs: '95%',
+        //   }
+        // }}
+      >
+        <Grid
+          container
+          sx={{
+            width: {
+              xs: '95%',
+              sm: '90%',
+              md: '85%',
+              lg: '80%',
+            },
+            display: 'flex',
+            justifyContent: {
+              xs: 'center',
+              sm: 'center',
+              md: 'space-between',
+            }
+          }}
+        >
+          <Grid item
+            sx={{
+              marginBottom: {
+                xs: '40px',
+              },
+              marginRight: {
+                sm: '40px',
+                lg: '0px',
+              }
+            }}
+          >
+            <Typography
+              color='#FFF'
+              sx={{
+                fontFamily: 'Noto Serif JP, serif',
+                fontSize: {
+                  xs: '16px',
+                  sm: '24px',
+                  lg: '40px',
+                }
+              }}
+            >
+              一流のビジネスマンのための <br />
+              一流のリーダーのための <br />
+              一流の経営者のためのタスク管理 <br />
+            </Typography>
+            <Box
+              sx={{
+                width: {
+                  xs: '136px',
+                },
+                paddingY: {
+                  xs: '16px',
+                }
+              }}
+            >
+              <CardMedia 
+                component="img"
+                image={AppLogo}
+              />
+            </Box>
+          </Grid>
+          <Grid 
+            item
+            sx={{
+              marginBottom: {
+                xs: '40px',
+              }
+            }}
+          >
+            {children}
+          </Grid>
+        </Grid>
+      </Box>
+      <Box
+        bgcolor="#2D2A2A"
+        display='flex'
+        justifyContent='center'
+      >
+        <Box
+          sx={{
+            width: {
+              xs: '95%',
+              sm: '90%',
+              md: '85%',
+              lg: '80%',
+            }
+          }}
+        >
+          <Footer />
+        </Box>
+      </Box>
     </>
   )
 }
