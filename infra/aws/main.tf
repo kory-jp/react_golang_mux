abe28908939d30b10bc7570598a264dbb7be66ca
kory-job <catcher.intherye.rk@gmail.com>
2022-06-10 16:34:46 +0900

    iam/S#

---
+++ infra/aws/main.tf
@@ -0,0 +1,38 @@
+# ---------------------------
+# Terraform configuration
+# ---------------------------
+
+terraform {
+  required_version = ">=1.2"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+#---------------------------
+# Provider
+#---------------------------
+
+provider "aws" {
+  region  = "ap-northeast-1"
+}
+
+#---------------------------
+#  Variable
+#---------------------------
+
+variable "name" {
+  type = string
+}
+
+variable "policy" {
+  type = string
+}
+
+variable "identifier" {
+  type = string
+}
+
