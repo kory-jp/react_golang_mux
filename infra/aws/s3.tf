783de9d81cbeb28e840aa16ffdde314af08c283e
kory-job <catcher.intherye.rk@gmail.com>
2022-06-19 15:10:42 +0900

    s3forImage

--- infra/aws/s3.tf
+++ infra/aws/s3.tf
@@ -1,6 +1,10 @@
+# ------------------------------
+# S3 for ALB_log
+# ------------------------------
+
 resource "aws_s3_bucket" "alb_log" {
-  bucket        = "alb-log-pragmatic-terraform-presiden-todo"
-  force_destroy = true
+  bucket = "alb-log-pragmatic-terraform-presiden-todo"
+  # force_destroy = true
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "bucket-config" {
@@ -31,4 +35,57 @@ data "aws_iam_policy_document" "alb_log" {
       identifiers = ["582318560864"]
     }
   }
+}
+
+# ------------------------------
+# S3 for Images
+# ------------------------------
+
+resource "aws_s3_bucket" "for_images" {
+  bucket = var.s3_images_bucket_name
+  # force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "for_images" {
+  bucket = aws_s3_bucket.for_images.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+data "aws_iam_policy_document" "allow_access_from_s3_user" {
+  version = "2012-10-17"
+
+  statement {
+    sid     = "statement1"
+    actions = ["s3:GetObject"]
+    effect  = "Allow"
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    resources = [
+      "arn:aws:s3:::${var.s3_images_bucket_name}/*"
+    ]
+  }
+
+  statement {
+    sid     = "2"
+    actions = ["s3:GetObject"]
+    effect  = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = [ "arn:aws:iam::${var.aws_account_id}:user${var.s3_path}${var.s3_user}" ]
+    }
+    resources = [
+      "arn:aws:s3:::${var.s3_images_bucket_name}/*"
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "for_images" {
+  bucket = aws_s3_bucket.for_images.id
+  policy = data.aws_iam_policy_document.allow_access_from_s3_user.json
 }
